dcc28ab309f0e09917e80dda832e6705d5b5894a
Md Mainuddin <mmainudd@gmail.com>
2015-03-12 23:43:38 +0600

    first commit

---
+++ custom.js
@@ -0,0 +1,81 @@
+$(document).ready(function () {
+    SetAddressAutoCompleteForControl(document.getElementById('companyaddress_1'));
+    $('#employment').on('click', '.add-btn', function () {
+        var html = "";
+        var i = $('.employment').size() + 1;
+        var newCompanyAddrID = 'companyaddress_' + i;
+        html += '<div class="employment"><h5 class="">Employment Info ' + i + '</h5><div class="form-group"><label class="col-md-4 control-label">Conpany Name<span>*</span></label><div class="col-md-8"><input  type="text" name="companyname[]" class="form-control"  value="" placeholder="Conpany Name" required/></div></div><div class="form-group"><label class="col-md-4 control-label">Conpany Address<span>*</span></label><div class="col-md-8"><input type="text" id="companyaddress_' + i + '" name="companyaddress[]" class="form-control"  value="" placeholder="Conpany Address" required/></div></div><div class="form-group"><label class="col-md-4 control-label">Phone Number<span>*</span></label><div class="col-md-8"><input  type="number" name="phonenumber[]" class="form-control"  value="" placeholder="Phone Number" required/></div></div><div class="remove-btn"><i class="glyphicon glyphicon-minus " ></i></div></div>';
+
+        $('#employment').append(html);
+        SetAddressAutoCompleteForControl(document.getElementById(newCompanyAddrID));
+        $('#jobApplication').find("#" + newCompanyAddrID).rules('add', {
+            required: true,
+            messages: {
+                required: 'Company Address is required'
+            }
+        });
+    });
+    $('#skill').on('click', '.add-btn', function () {
+        var html = "";
+        var i = $('.skill').size() + 1;
+        html += '<div class="skill"><h5 class="">Skill Info ' + i + '</h5><div class="form-group"><label class="col-md-4 control-label">Skill Name<span>*</span></label><div class="col-md-8"><input  type="text" name="skillname[]" class="form-control"  value="" placeholder="Skill Name" required/></div></div><div class="form-group"><label class="col-md-4 control-label">Year Of Experience<span>*</span></label><div class="col-md-8"><select id="skillexp_' + i + '" name="skillexp[]" class="form-control" required></select></div></div><div class="remove-btn"><i class="glyphicon glyphicon-minus " ></i></div></div>';
+
+        $('#skill').append(html);
+        $('#skillexp_' + i).html($('#skillexp_1').html());
+    });
+    $('#employment,#skill').on('click', '.remove-btn', function () {
+        $(this).parent().remove();
+    });
+
+
+    $('#jobApplication').validate({
+        rules: {
+            email: {
+                required: true,
+                email: true
+            },
+            confirmemail: {
+                required: true,
+                email: true,
+                equalTo: ".email"
+            }
+
+        },
+        messages: {
+        },
+        submitHandler: function (form) {
+            $.ajax({
+                type: "post",
+                url: "action.php",
+                data: $(form).serializeArray(),
+                success: function () {
+
+                }
+            });
+        }
+    });
+});
+
+function SetAddressAutoCompleteForControl(input) {
+    //Fix for Firefox.  Enter a location was not hiding on focus.
+    $(input).focus(function () {
+        if ($(this).val() == "Enter a location") {
+            $(this).val('');
+        }
+    });
+
+    lat = 40.4136377;
+    long = -30.7290164;
+    var defaultBounds = new google.maps.LatLngBounds(new google.maps.LatLng(lat, long));
+    var options = {bounds: defaultBounds, types: ['geocode', 'establishment']};
+    var autocomplete = new google.maps.places.Autocomplete(input, options);
+
+    google.maps.event.addListener(autocomplete, 'place_changed', function () {
+        var place = autocomplete.getPlace();
+        var address = place.formatted_address;
+        $(input).val(address);
+        //////SUBMIT FORM
+//        var lat_address = place.geometry.location.lat();
+//        var lng_address = place.geometry.location.lng();
+    });
+}
---
+++ index.php
@@ -0,0 +1,161 @@
+<?php
+$months = array(
+    1 => 'January',
+    2 => 'February',
+    3 => 'March',
+    4 => 'April',
+    5 => 'May',
+    6 => 'June',
+    7 => 'July',
+    8 => 'August',
+    9 => 'September',
+    10 => 'October',
+    11 => 'November',
+    12 => 'December'
+);
+?>
+<!DOCTYPE HTML>
+<html lang="en-US">
+    <head>
+        <title>Job Application form</title>
+        <meta charset="UTF-8">
+
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+        <link rel="stylesheet" href="style.css">
+
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
+
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+        <script src="https://maps.google.com/maps/api/js?v=3.13&sensor=false&libraries=places" type="text/javascript"></script>
+        <script src="jquery.validate.min.js"></script>
+        <script src="custom.js"></script>
+
+    </head>
+    <body>
+
+        <div class="container">
+            <div class="myForm">
+                <div class="col-md-12 form-heading">Job Application Form</div>
+                <form id="jobApplication" class="form-horizontal" name="jobApplication" action="" method="POST" onsubmit="return false">
+                    <div class="form-group">
+                        <label class="col-md-4 control-label">First Name<span>*</span></label>
+                        <div class="col-md-8">
+                            <input  type="text" name="firstname" class="form-control" value="" placeholder="First Name" required/>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="col-md-4 control-label">Last Name<span>*</span></label>
+                        <div class="col-md-8">
+                            <input  type="text" name="lasttname" class="form-control"  value="" placeholder="Last Name" required/>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="col-md-4 control-label">Place of birth<span>*</span></label>
+                        <div class="col-md-8">
+                            <input  type="text" name="birthplace" class="form-control"  value="" placeholder="Place of birth" required/>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="col-md-4 control-label">Date of birth<span>*</span></label>
+                        <div class="col-md-8">
+                            <div class="birthday">
+                                <select name="birthday" class="form-control" required>
+                                    <?php for ($i = 1; $i <= 31; $i++) { ?>
+                                        <option value="<?php echo $i; ?>" ><?php echo $i; ?></option>
+                                    <?php } ?>
+                                </select>
+                            </div>
+                            <div class="birthday">
+                                <select name="birthmonth" class="form-control" required>
+                                    <?php foreach ($months as $key => $value) { ?>
+                                        <option value="<?php echo $key; ?>" ><?php echo $value; ?></option>
+                                    <?php } ?>
+                                </select>
+                            </div>
+                            <div class="birthday">
+                                <select name="birthyear" class="form-control" required>
+                                    <?php for ($i = date('Y') - 1; $i >= 1950; $i--) { ?>
+                                        <option value="<?php echo $i; ?>" ><?php echo $i; ?></option>
+                                    <?php } ?>
+                                </select>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="col-md-4 control-label">Current Address<span>*</span></label>
+                        <div class="col-md-8">
+                            <input  type="text" name="currentaddress" class="form-control"  value="" placeholder="Current Address" required/>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="col-md-4 control-label">Email Address<span>*</span></label>
+                        <div class="col-md-8">
+                            <input  type="email" name="email" class="form-control email"  value="" placeholder="Email Address" required/>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="col-md-4 control-label">Confirm Email<span>*</span></label>
+                        <div class="col-md-8">
+                            <input  type="email" name="confirmemail" class="form-control"  value="" placeholder="Confirm Email Address" required/>
+                        </div>
+                    </div>
+                    <div id="employment">
+                        <h4 class="heading">Employment Info</h4>
+                        <div class="employment">
+                            <div class="form-group">
+                                <label class="col-md-4 control-label">Conpany Name<span>*</span></label>
+                                <div class="col-md-8">
+                                    <input  type="text" name="companyname[]" class="form-control"  value="" placeholder="Conpany Name" required/>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="col-md-4 control-label">Conpany Address<span>*</span></label>
+                                <div class="col-md-8">
+                                    <input type="text" id="companyaddress_1" name="companyaddress[]" class="form-control"  value="" placeholder="Conpany Address" required/>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="col-md-4 control-label">Phone Number<span>*</span></label>
+                                <div class="col-md-8">
+                                    <input  type="number" name="phonenumber[]" class="form-control"  value="" placeholder="Phone Number" required/>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="add-btn"><i class="glyphicon glyphicon-plus " ></i></div>
+                    </div>
+                    <div id="skill">
+                        <h4 class="heading">Skill Info</h4>
+                        <div class="skill">
+                            <div class="form-group">
+                                <label class="col-md-4 control-label">Skill Name<span>*</span></label>
+                                <div class="col-md-8">
+                                    <input  type="text" name="skillname[]" class="form-control"  value="" placeholder="Skill Name" required/>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="col-md-4 control-label">Year Of Experience<span>*</span></label>
+                                <div class="col-md-8">
+                                    <select id="skillexp_1" name="skillexp[]" class="form-control" required>
+                                        <?php for ($i = 1; $i <= 20; $i++) { ?>
+                                            <option value="<?php echo $i; ?>" ><?php echo $i; ?> Year</option>
+                                        <?php } ?>
+                                    </select>
+                                </div>
+                            </div>
+
+                        </div>
+                        <div class="add-btn"><i class="glyphicon glyphicon-plus " ></i></div>
+                    </div>
+                    <div class="form-group">
+                        <input  type="submit" name="submit" value="Apply" class="btn btn-default submit-btn"/>
+
+                    </div>
+                </form>
+            </div>
+        </div>
+    </body>
+</html>
